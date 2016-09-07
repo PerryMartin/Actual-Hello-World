56fef640278cbc09ae2028f417cfef7775d22a89
PerryMartin <perry.martin@ocsbstudent.ca>
2016-09-07 14:39:57 -0400

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,3 +1,11 @@
+/******************************************************************************
+ *
+ * Created by: Perry Martin
+ * Created on: Sept 7 2016
+ * This program is the traditional Hello, World! first program.
+ *
+ ******************************************************************************/
+
 public class HelloWorld {
 
  public static void main(String[] args)
